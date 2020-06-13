76651952b9be0f3dad4d6ed19c6ead85bf76c915
Govind <Govind@DESKTOP-HQCM5R0>
2020-06-13 20:54:55 +0530

    Git Jenkins Integration push

---
+++ GitJenkins/src/pkg/gitjen.java
@@ -0,0 +1,15 @@
+package pkg;
+
+import org.testng.annotations.Test;
+
+public class gitjen {
+
+@Test
+
+public void display()
+{
+System.out.println("This is Github Jenkins Integration Testing");	
+}
+
+
+}
